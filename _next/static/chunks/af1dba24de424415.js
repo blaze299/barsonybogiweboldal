de13d5690870ae6c24718344ed6b6251cada0594
fazekasluca2007 <fazekasl@kkszki.hu>
2026-03-19 20:39:43 +0100

    a

---
+++ _next/static/chunks/af1dba24de424415.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/f782ab51ccbe513b.js",
+  "static/chunks/turbopack-617039faa57d8d36.js"
+])
---
+++ _next/static/chunks/e1d35a6b3b206ef5.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/f782ab51ccbe513b.js",
+  "static/chunks/turbopack-84fdc763206c2662.js"
+])
